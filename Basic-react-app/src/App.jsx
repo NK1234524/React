baec580b6c39ad9aefde1d0e607f519b24c272ec
Nishant kumar <nishant201kashyap@gmail.com>
2025-08-10 14:24:44 +0530

    add

--- Basic-react-app/src/App.jsx
+++ Basic-react-app/src/App.jsx
@@ -4,14 +4,7 @@ function Title(){
   return <p>Hello everyone</p>;
 }
 function App() {
-  return (
-  
-  <div>
-    <h2><button>Hello world</button></h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque obcaecati dignissimos nam accusantium dolores facilis expedita amet facere blanditiis, debitis necessitatibus corrupti natus cum commodi voluptatem adipisci, laboriosam velit nobis?</p>
-    <Title/>
-  </div>
-  );
+  return <Title/>;
 }
 
 export default App;
