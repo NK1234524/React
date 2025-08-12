3c3c84f18589297f485aa4f350e23bb40250382d
Nishant kumar <nishant201kashyap@gmail.com>
2025-08-12 15:38:00 +0530

    add

---
+++ src/Title.jsx
@@ -0,0 +1,9 @@
+function Title(){
+return (<div className="Main">
+    <h4>I am the Title2!</h4>
+    <p>2*2 = {2*2}</p>
+    <p>2*3 = {2*3}</p>
+    <button>Click for count</button>
+</div>)
+}
+export default Title ;
